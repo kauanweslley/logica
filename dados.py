55101e44594217b73033e1e989d923851b524657
kauanweslley <50590816+kauanweslley@users.noreply.github.com>
2019-05-13 14:20:07 -0300

    Dados: calcular os dados,soma e empate

--- dados.py
+++ dados.py
@@ -1,16 +1,32 @@
+# Meu primeiro programa
 from random import randint  # IMPORTA A FUNÇÃO DE GERAR NUMEROS ALEATORIOS
 
-print("Olá 1info2!")
- 
-dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
-print("Dado 1:", dado1) 
+print("Jogo dos dados:")
 
-dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
-print("Dado 2:", dado 2)
+dado1 = randint(1, 6)  # gera um numero entre 1 e 6 aleatoriamente
+print("Dado 1:", dado1)
 
-dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
-print("Dado 3:", dad 3)
+dado2 = randint(1, 6)  # gera um numero entre 1 e 6 aleatoriamente
+print("Dado 2:", dado2)
 
-dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
-print("Dado 4:", dado 4)
+dado3 = randint(1, 6)  # gera um numero entre 1 e 6 aleatoriamente
+print("Dado 3:", dado3)
 
+dado4 = randint(1, 6)  # gera um numero entre 1 e 6 aleatoriamente
+print("Dado 4:", dado4)
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print('jogador 1:', jogador1)
+print('jogador 2:', jogador2)
+
+if jogador1 > jogador2: 
+    print('jogador 1 venceu')
+else:
+    if jogador2 > jogador1:
+        print('jogador 2 venceu')
+    else:
+        print("Empate!")
+
+    
