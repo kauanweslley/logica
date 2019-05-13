2a23c3642914b6ba50864042e10481379bd0fb26
kauanweslley <50590816+kauanweslley@users.noreply.github.com>
2019-05-13 13:43:53 -0300

    Dados: nome do autor e outros 3 dados

--- dados.py
+++ dados.py
@@ -3,4 +3,14 @@ from random import randint  # IMPORTA A FUNÇÃO DE GERAR NUMEROS ALEATORIOS
 print("Olá 1info2!")
  
 dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
-print("Dado 1:", dado1)
+print("Dado 1:", dado1) 
+
+dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
+print("Dado 2:", dado 2)
+
+dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
+print("Dado 3:", dad 3)
+
+dado1 = randint(1, 6) # gera um numero entre 1 e 6 aleatoriamente 
+print("Dado 4:", dado 4)
+
