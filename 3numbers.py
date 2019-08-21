a8ee7fc95393cf13a6f76becb5eadb3b4272b24c
kauanweslley <50590816+kauanweslley@users.noreply.github.com>
2019-08-21 14:22:25 -0300

    Add files via upload

---
+++ 3numbers.py
@@ -0,0 +1,19 @@
+# Faça um programa que leia três numeros e mostre o maior e o menor deles 
+
+numero1 = int(input("numero 1: "))
+numero2 = int(input("numero 2: "))
+numero3 = int(input("numero 3: "))
+
+if numero1 < numero2 and numero1 < numero3:
+    print("%d é o menor" %numero1)
+elif numero2 < numero1 and numero2 < numero3:
+    print("%d é o menor" %mumero2)
+else:
+    print("%d é o menor" %numero3)
+
+if numero1 > numero2 and numero1 > numero3:
+    print("%d é o maior" %numero1)
+elif numero2 > numero1 and numero2 > numero3:
+    print("%d é o maior" %mumero2)
+else:
+    print("%d é o maior" %numero3)
